80256a09512e29230c7dd59d9c69480e1fa92a4b
Diki Siswanto <dickyachmady@gmail.com>
2021-11-08 16:49:27 +0800

    initial layout

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,65 @@
-const a = 10;
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+// Set up the chart
+$(document).ready(() => {
+  $('table.datatables').DataTable({
+    language: {
+      url: 'http://cdn.datatables.net/plug-ins/1.10.18/i18n/Indonesian.json',
+    },
+    dom: 'Bfrtip',
+    buttons: [
+      'excelHtml5',
+      'csvHtml5',
+      {
+        extend: 'pdfHtml5',
+        orientation: 'landscape',
+        pageSize: 'A4',
+      },
+      'print',
+      {
+        extend: 'colvis',
+        columns: ':gt(2)',
+      },
+    ],
+  });
+
+  const chart = new Highcharts.Chart({
+    chart: {
+      renderTo: 'chart-container',
+      type: 'column',
+      options3d: {
+        enabled: true,
+        alpha: 15,
+        beta: 15,
+        depth: 50,
+        viewDistance: 25,
+      },
+    },
+    title: {
+      text: 'Anggaran Pendapatan dan Belanja  - Kabupaten BANTUL',
+    },
+    yAxis: {
+      min: 0,
+      title: { text: 'Nominal (dlm Juta Rupiah)' },
+    },
+    xAxis: {
+      categories: ['2018', '2017'],
+    },
+    plotOptions: {
+      column: {
+        depth: 25,
+      },
+    },
+    series: [
+      {
+        name: 'BELANJA',
+        data: [2874747.56, 2161336.56],
+      }, {
+        name: 'PENDAPATAN',
+        data: [2078242.75, 2078242.75],
+      }, {
+        name: 'PENERIMAAN PEMBIAYAAN DAERAH',
+        data: [103203.81, 103203.81],
+      }],
+  });
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,71 @@
+/* eslint-disable global-require */
 module.exports = {
   mode: 'jit',
   purge: ['./build/*.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        main: ['Lato', 'sans-serif'],
+      },
+    },
   },
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('daisyui'),
+  ],
+  daisyui: {
+    themes: [{
+      flamboyan: {
+        primary: '#EF4444',
+        /* Primary color */
+        'primary-focus': '#DC2626',
+        /* Primary color - focused */
+        'primary-content': '#ffffff',
+        /* Foreground content color to use on primary color */
+
+        secondary: '#3B82F6',
+        /* Secondary color */
+        'secondary-focus': '#2563EB',
+        /* Secondary color - focused */
+        'secondary-content': '#ffffff',
+        /* Foreground content color to use on secondary color */
+
+        accent: '#37cdbe',
+        /* Accent color */
+        'accent-focus': '#2aa79b',
+        /* Accent color - focused */
+        'accent-content': '#ffffff',
+        /* Foreground content color to use on accent color */
+        neutral: '#3d4451',
+        /* Neutral color */
+        'neutral-focus': '#2a2e37',
+        /* Neutral color - focused */
+        'neutral-content': '#ffffff',
+        /* Foreground content color to use on neutral color */
+
+        'base-100': '#ffffff',
+        /* Base color of page, used for blank backgrounds */
+        'base-200': '#f9fafb',
+        /* Base color, a little darker */
+        'base-300': '#d1d5db',
+        /* Base color, even more darker */
+        'base-content': '#ffffff',
+        /* Foreground content color to use on base color */
+
+        info: '#2094f3',
+        /* Info */
+        success: '#009485',
+        /* Success */
+        warning: '#ff9900',
+        /* Warning */
+        error: '#ff5724',
+      },
+    },
+    'bumblebee',
+    'emerald',
+    ],
+  },
 };
